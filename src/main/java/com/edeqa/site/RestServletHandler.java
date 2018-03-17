9e7e23a36a909dc94897664a6af92bc885661ef4
Tujger <tujger@gmail.com>
2018-03-17 17:15:01 -0400

    EQ-18 EdequateServer

--- src/main/java/com/edeqa/site/RestServletHandler.java
+++ src/main/java/com/edeqa/site/RestServletHandler.java
@@ -1,6 +1,8 @@
 package com.edeqa.site;
 
 
+import com.edeqa.site.rest.Test;
+
 import javax.servlet.ServletException;
 
 public class RestServletHandler extends com.edeqa.edequate.RestServletHandler {
@@ -9,7 +11,9 @@ public class RestServletHandler extends com.edeqa.edequate.RestServletHandler {
     public void init() throws ServletException {
         super.init();
         useDefault();
-        populateRestActions(getClass().getPackage().getName() + ".rest");
+//        populateRestActions(getClass().getPackage().getName() + ".rest");
+        registerAction(new Test());
+        registerActionsPool();
     }
 
 }
